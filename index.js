20df21f12b7a5e95289a88174ef7f94602dfd86c
Sam Oaks <samuelclayoaks@yahoo.com>
2022-12-28 19:27:41 -0500

    added some code to index.js

--- index.js
+++ index.js
@@ -1,13 +1,90 @@
 // TODO: Include packages needed for this application
+const { default: inquirer } = require("inquirer");
+const fs =require('fs');
 
+function createHTML(userinfo) {
+    const {
+        Title,
+        location,
+        bio,
+        linkedInURL,
+        GitHubURL,
+    } = userinfo;
+
+    return ` 
+    # ${Title}
+    
+    # Description
+    ${Description}
+
+    # Table of Contents
+
+    # Installation
+
+    # Usage
+
+    # License
+
+    # Contributing
+
+    # Tests
+
+    # Questions
+
+    `
+}
 // TODO: Create an array of questions for user input
-const questions = [];
+function askQuestions() {
+    return inquirer.prompt([
+        {
+            name: `Title`,
+            message: 'What is the name of your Project?',
+            type: 'input'
+        },
+        {
+            name: `Descrpition`,
+            message: 'Please write a discription of your project',
+            type: 'editor'
+        },
+        {
+            name: `Installation`,
+            message: 'Please enter instalation intructions',
+            type: 'editor'
+        },
+        {
+            name: `Usage`,
+            message: 'please enter usage information',
+            type: 'editor'
+        },
+        {
+            name: `contribution guidlines`,
+            message: 'enter contribution guidlines',
+            type: 'editor'
+        },
+        {
+            name: `test instrucions`,
+            message: 'please enter test instructions',
+            type: 'editor'
+        },
+    ]);
+};
+//const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(md) {
+    fs.writeFile('README.md', md, (err) => {
+        err
+        ? console.log(err)
+        : console.Console.log('done');
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    askQuestions()
+    .then(createREADME)
+    .then(writeToFile)
+}
 
 // Function call to initialize app
 init();
